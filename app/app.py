2c6259056c22907e850163ebbed6a1a0fe72d9f5
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-06-27 17:11:57 -0300

    Se conecto la base de datos SQL, se creo la pagina de registro, se creo la funcion de registro en BD.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from datetime import datetime
 
 app = Flask(__name__, static_folder='../static')
+
+#CONFIGURACION DE LA BASE DE DATOS
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:lucas1889@localhost/rosario_cursos'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route('/usuarios')
+def mostrar_usuarios():
+    resultado = db.session.execute(text('SELECT * FROM usuarios'))
+    usuarios = resultado.fetchall()
+    return str(usuarios)  
 
 @app.route('/') 
 def index():
@@ -11,5 +27,30 @@ def index():
 def login():
     return render_template('/contactoForm.html')
 
+@app.route('/registro', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+            nombre = request.form['name']
+            apellido = request.form['apellido']
+            email = request.form['email']
+            contrasena = request.form['contrasena']
+            documento = request.form['documento']
+           
+            insert_query = text("""
+                INSERT INTO usuarios
+                VALUES(:DNI, :nombre, :apellido, :email, :contrasena, :fecha_registro, :rol)
+            """)
+            db.session.execute(insert_query, {
+                 'DNI': documento,
+                 'nombre': nombre,
+                 'apellido': apellido,
+                 'email': email,
+                 'contrasena': contrasena,
+                 'fecha_registro': datetime.now().date(),
+                 'rol': 'alumno'
+            })
+            db.session.commit()
+            return "¡Datos recibidos!"
+    return render_template('/register.html')
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
