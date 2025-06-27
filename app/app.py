5be684307dd3d9c38ccb3e5c7b59a806e0007c15
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-06-27 15:41:26 -0300

    Se solucionaron los problemas de enrutado con flask

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static')
 
 @app.route('/') 
 def index():
     # return 'Hola, desde FLASK!'
     return render_template('index.html')
 
+@app.route('/contactoForm')
+def login():
+    return render_template('/contactoForm.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
