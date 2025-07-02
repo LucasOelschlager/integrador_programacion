89012ef7c5a78ea73dc8e591f7a79c62c50d32ac
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-07-02 10:56:36 -0300

    Se borraron carpetas en desuso

--- static/logic/render_curso.js
+++
@@ -1,9 +0,0 @@
-const cards_container = document.querySelector(".container");
-let element;
-cards_container.addEventListener("click", (e) => {
-  const focus = e.target;
-  if (focus.classList.contains("button")) {
-    element = focus.parentElement;
-    renderCurso(element.id);
-  }
-});
